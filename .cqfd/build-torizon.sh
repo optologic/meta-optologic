4f5054f5f8b44d00b37286c6ea5404db242dbf7f
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2026-04-27 17:32:48 +0200

    build-torizon: allow building multiple machine and display combinations
    As all the displays on the imx8mp require the same kernel patch, change
    the build script to allow building images for multiple display types by
    passing the display type as an argument. This allows us to build images
    for the 7inch and 10inch displays in addition to the 5inch display.
    
    SFL: #22118
    Change-Id: I5c491752d5a5a9bdf1c758898115f98a1ff3d866
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

--- .cqfd/build-torizon.sh
+++ .cqfd/build-torizon.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 # CI Bitbake script for meta-optologic CI
-# Builds the binary Torizon BSP image for verdin-imx8mp + 5inch display using the SSTATE cache on our self-hosted runner
-# Usage: cqfd -f layers/meta-optologic/.cqfdrc -d layers/meta-optologic/.cqfd run layers/meta-optologic/.cqfd/build.sh
+# Builds the binary Torizon BSP images for verdin-imx8mp using the SSTATE cache on our self-hosted runner
+# Usage: cqfd -f layers/meta-optologic/.cqfdrc -d layers/meta-optologic/.cqfd run layers/meta-optologic/.cqfd/build.sh <machine> <display>
 
 set -exo pipefail
 
 # Prepare environment variables
+if [ -n "$1" ]; then
+    MACHINE="$1"
+else
+    MACHINE="verdin-imx8mp"
+fi
+
+if [ -n "$2" ]; then
+    DISPLAY="$2"
+else
+    DISPLAY="5inch-SCX0500132GGC06"
+fi
+
+export DISPLAY
 # DL_DIR and SSTATE_DIR may be configured in the bash environment to speed up the build
 export DL_DIR
 export SSTATE_DIR
 export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS \
     SSTATE_DIR \
     DL_DIR \
+    DISPLAY \
 "
 
 # Cleanup build directory (we need a clean bblayers.conf)
 rm -rf build
 
 # Initialize Torizon Build environment
-MACHINE=verdin-imx8mp EULA=1 source setup-environment ./build
+MACHINE=${MACHINE} EULA=1 source setup-environment ./build
 
 # Add meta-optologic layers
 bitbake-layers add-layer \
     ../layers/meta-optologic/meta-optologic-tdx-common \
     ../layers/meta-optologic/meta-optologic-tdx-nxp
 
-# Load the 5inch panel dtbo at boot time (the only one that we need the Torizon kernel patch for)
-echo 'OPTOLOGIC_DEVICETREE_OVERLAYS_BOOT = "${MACHINE}_optologic_panel-cap-touch-5inch-lvds_overlay.dtbo"' >> conf/local.conf
+# Load the panel dtbo at boot time (the only one that we need the Torizon kernel patch for)
+echo 'OPTOLOGIC_DEVICETREE_OVERLAYS_BOOT = "${MACHINE}_optologic_panel-cap-touch-${DISPLAY}-lvds_overlay.dtbo"' >> conf/local.conf
+echo 'DISTRO_FLAVOUR = " Toradex ${MACHINE} with OPTO Logic ${DISPLAY} Display and Touchscreen"' >> conf/local.conf
 
 # Run bitbake
 bitbake -k torizon-docker
